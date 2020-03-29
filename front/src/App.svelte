975da5b8f8a7f60079e283de0171ba3fa359b3ca
Bvlion <bvltiggeari@ambitious-i.net>
2020-03-29 19:05:53 +0900

    退避

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -2,6 +2,8 @@
   import Router from 'svelte-spa-router'
   import Home from './routes/home.svelte'
   import Showpage from './routes/showpage.svelte'
+  import HeaderLink from './component/header_link.svelte'
+  import {link} from 'svelte-spa-router' 
   
   const routes = {
       '/': Home,
@@ -9,51 +11,38 @@
   }
 </script>
 
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <!-- Title -->
-      <span class="mdl-layout-title">Title</span>
-      <!-- Add spacer, to align navigation to the right -->
-      <div class="mdl-layout-spacer"></div>
-      <!-- Navigation. We hide it in small screens. -->
-      <nav class="mdl-navigation mdl-layout--large-screen-only">
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-  </header>
-  <div class="mdl-layout__drawer">
+<header class="mdl-layout__header">
+  <div class="mdl-layout__header-row">
     <span class="mdl-layout-title">Title</span>
-    <nav class="mdl-navigation">
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
-      <a class="mdl-navigation__link" href="">Link</a>
+    <div class="mdl-layout-spacer"></div>
+    <nav class="mdl-navigation mdl-layout--large-screen-only">
+      <HeaderLink logined="" />
     </nav>
   </div>
-  <main class="mdl-layout__content">
-    <div class="page-content"><Router {routes}/></div>
-  </main>
-  <footer class = "mdl-mini-footer">
-    <div class = "mdl-mini-footer__left-section">
-     <div class = "mdl-logo">
-      Copyright Information
-     </div>
-     <ul class = "mdl-mini-footer__link-list">
-      <li><a href = "#">Help</a></li>
-      <li><a href = "#">Privacy and Terms</a></li>
-      <li><a href = "#">User Agreement</a></li>
-     </ul>
-    </div>
-     
-    <div class = "mdl-mini-footer__right-section">
-     <button class = "mdl-mini-footer__social-btn">1</button>
-     <button class = "mdl-mini-footer__social-btn">2</button>
-     <button class = "mdl-mini-footer__social-btn">3</button>
-    </div>
-  
-  </footer>
+</header>
+<div class="mdl-layout__drawer">
+  <span class="mdl-layout-title">Title</span>
+  <nav class="mdl-navigation">
+    <HeaderLink logined=false />
+  </nav>
 </div>
+<main class="mdl-layout__content">
+  <div class="page-content"><Router {routes}/></div>
+</main>
+<footer class = "mdl-mini-footer">
+  <div class = "mdl-mini-footer__left-section">
+   <div class = "mdl-logo">
+    Copyright Information
+   </div>
+   <ul class = "mdl-mini-footer__link-list">
+    <li><a href = "/sitemap" use:link>サイトマップ</a></li>
+    <li><a href = "/privacy" use:link>プライバシーポリシー</a></li>
+    <li><a href = "/rule" use:link>利用規約</a></li>
+   </ul>
+  </div>
+  <div class = "mdl-mini-footer__right-section">
+   <button class = "mdl-mini-footer__social-btn">1</button>
+   <button class = "mdl-mini-footer__social-btn">2</button>
+   <button class = "mdl-mini-footer__social-btn">3</button>
+  </div>
+</footer>
---
+++ front/src/component/header_link.svelte
@@ -0,0 +1,16 @@
+
+<script>
+  import {link} from 'svelte-spa-router'
+  export let logined
+</script>
+
+{#if logined}
+{logined}
+<a class="mdl-navigation__link" href="/webhooks" use:link>WebHook 一覧</a>
+{:else}
+{logined}
+<a class="mdl-navigation__link" href="/login" use:link>ログイン</a>
+{/if}
+<a class="mdl-navigation__link" href="/qa" use:link>Q & A</a>
+<a class="mdl-navigation__link" href="/notices" use:link>お知らせ</a>
+<a class="mdl-navigation__link" href="/inquiry" use:link>お問い合わせ</a>
